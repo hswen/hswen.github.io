04140a21b3dc9ed70b1f09abe1c046386ee904e2
Hswen <hsiawu@umail.iu.edu>
2014-03-23 13:58:13 -0400

    edit js files

--- js/tablink.js
+++ js/tablink.js
@@ -1,8 +1,4 @@
- var url = document.location.toString(); 
-if (url.match('#')) { 
-    $('#myTabs a[href=#'+url.split('#')[1]+']').tab('show') ; 
-} 
-// Change hash for page-reload 
-$('#myTabs a').on('shown', function (e) { 
-    window.location.hash = e.target.hash; 
-});// JavaScript Document
+$('#myTabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+});
